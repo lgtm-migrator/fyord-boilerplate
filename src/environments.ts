188336ccb4dee25a7870d4dd68ea7b440ca48869
Joey Bayes <joey.bayes@uscca.com>
2021-04-09 23:39:39 -0400

    Update to pass in environment variables in index bootstrap call.

---
+++ src/environments.ts
@@ -0,0 +1,16 @@
+import { Environments } from 'fyord';
+
+export enum EnvironmentVariables {
+  Mode = 'mode',
+  ApiServer = 'apiServer'
+}
+
+export const DevelopmentEnvironmentVariables = new Map<string, string>([
+  [EnvironmentVariables.Mode, Environments.Development],
+  [EnvironmentVariables.ApiServer, 'http://localhost:5000']
+]);
+
+export const ProductionEnvironmentVariables = new Map<string, string>([
+  [EnvironmentVariables.Mode, Environments.Production],
+  [EnvironmentVariables.ApiServer, 'http://www.example.com/api']
+]);
