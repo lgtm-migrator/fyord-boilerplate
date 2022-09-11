8afa7c392c46b348f29f93a614f42c30a5795b3a
Joey Bayes <joey.bayes@uscca.com>
2022-09-11 12:32:30 -0400

    Remove concurrently in favor of webpack shell plugin.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,18 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+  plugins: [
+    new WebpackShellPlugin({
+      onBuildStart: [
+        'tsc -w ./src/electron/*.ts --outDir ./public'
+      ],
+      onBuildEnd: [
+        'electron public/main.js'
+      ]
+    })
+  ]
 });
